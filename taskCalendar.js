e26b35d4d7e43632439dccee94f675d9acce0ebe
DE GUIA, MARK DANIEL C. <markdanieldeguia0@gmail.com>
2025-03-06 22:12:23 +0800

    Add files via upload

---
+++ taskCalendar.js
@@ -0,0 +1,30 @@
+// prompt the user to enter the day of the week
+let dayInWeek = prompt("Enter the day of the week (note: All lowerCase): ")
+
+// Check the day and assign a task
+switch (dayInWeek) {
+    case "monday":
+        console.log("Your task for Mondat is: Finish your assignments.")
+        break;
+    case "tuesday":
+        console.log("Your task for Tuesday is: Go to the gym.")
+        break;
+    case "wednesday":
+        console.log("Your task for Wednesday is: Attend your online class.")
+        break;
+    case "thursday":
+        console.log("Your task for Thursday is: Finish your project.")
+        break;
+    case "friday":
+        console.log("Your task for Friday is: Submit your assignment.")
+        break;
+    case "saturday":
+        console.log("Your task for Saturday is: take a break.")
+        break;
+    case "sunday":
+        console.log("Your task for Sunday is: Attend church service.")
+        break;
+    default:
+        console.log("Invalid day.");
+        break;
+}
